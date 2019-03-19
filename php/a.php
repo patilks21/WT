6f6fc694ec93921857ca505da3e2ee1e93e4c62f
patilks21 <46299620+patilks21@users.noreply.github.com>
2019-03-19 10:02:08 +0530

    insert delete select

---
+++ php/a.php
@@ -0,0 +1,48 @@
+<?php	
+	$servername="localhost";
+	$username="root";
+	$password="mysql";
+	$db="tecob230";
+	$conn=mysqli_connect($servername,$username,$password,$db);
+	if(!$conn)
+	{
+		echo("conn failed".mysqli_connect_error);
+	}
+	else
+	{
+		echo("ok");
+
+	}
+	$uname=$_POST["username"];
+	$pass=$_POST["password"];
+	$action=$_POST["action"];
+	if($action=='insert')
+	{
+		$sql="insert into log values('".$uname."','".$pass."');";
+		if($conn->query($sql)===TRUE)
+		{
+			echo ("inserted");
+		}
+		else
+		{
+			echo "\nnot done";
+		}
+	}
+	else if($action=='delete')
+	{
+		$sql="Delete from log where username='".$uname."';";
+		$conn->query($sql);
+		echo $action;
+	}
+	else if($action=='select')
+	{
+		$sql="select * from log;";
+		$result= $conn->query($sql);
+		while($row=$result->fetch_array())
+		{
+			echo("<hr>username: ".$row["username"]."<br>password: ".$row["password"]."<br>");
+		}
+	}
+?>
+
+
